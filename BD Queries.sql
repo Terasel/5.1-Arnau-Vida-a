384a7b8cd555d6f4e2e6b6ea307e8c65e7a8cd7e
Arnau Vidaña Garcia <arnauvidana95@gmail.com>
2024-07-29 10:20:42 +0200

    BD Queries v1.1

--- BD Queries.sql
+++ BD Queries.sql
@@ -5,5 +5,5 @@ SELECT * FROM `ulleres` WHERE venedor = 'Elrond Halfelven' AND data_venda BETWEE
 SELECT DISTINCT p.nom AS 'Proveïdors' FROM `proveidors` p JOIN `marques` m ON p.id_proveidor = m.id_proveidor JOIN `ulleres` u ON m.id_marca = u.id_marca WHERE u.id_ulleres IS NOT NULL;
 -- Pizzeria
 USE `coruscant_pizza`;
-SELECT c.quantitat_begudes AS 'Quantitat Begudes', b.localitat AS 'Localitat' FROM `comandes` c JOIN `botigues` b ON c.id_botiga = b.id_botiga WHERE b.localitat = 'Mos Espa';
-SELECT c.id_comanda AS 'ID Comanda', CONCAT(e.nom,' ',e.cognoms) AS 'Empleat' FROM `comandes` c JOIN `empleats` e ON c.id_repartidor = e.id_empleat WHERE e.nom = "Anami";
+SELECT SUM(c.quantitat_begudes) AS 'Quantitat Begudes venudes a Mos Espa' FROM `comandes` c JOIN `botigues` b ON c.id_botiga = b.id_botiga WHERE b.localitat = 'Mos Espa';
+SELECT COUNT(c.id_comanda) AS 'Quantitat de comandes efectuades per Anami Brene Rastee' FROM `comandes` c JOIN `empleats` e ON c.id_repartidor = e.id_empleat WHERE e.nom = "Anami";
